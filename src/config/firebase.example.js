ea72c3aa17cd3c31cc536ba8e1016fb0affe3712
Dhairya Patel <dhairya.patel@iqinfinite.in>
2024-11-13 11:57:40 +0530

    firebase example

---
+++ src/config/firebase.example.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  // Replace with your Firebase config
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app-id",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
